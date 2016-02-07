184d1e67385038cff8c4f05fe70a57ee9b36eaf8
Kirk Walker Graves <graves.kirk@gmail.com>
2016-02-07 14:52:04 -0600

    created seed files for application and installed dependencies

---
+++ app/app.js
@@ -0,0 +1,15 @@
+(function() {
+	var app = angular.module('BooksApp', ['ui.router', 'ui.bootstrap', 'ngMaterial']);
+
+	app.config(['$stateProvider', '$urlRouterProvider', '$mdThemingProvider', 
+		function($stateProvider, $urlRouterProvider, $mdThemingProvider) {
+		$urlRouterProvider.otherwise('/');
+
+		$stateProvider
+		.state('search', {
+			url: '/search',
+			templateUrl: 'partials/search.html',
+			controller: 'SearchCtrl'
+		});
+	}]);
+})();
