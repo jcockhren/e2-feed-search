1384a5b6301c30e1fc267039587f0c6d439d9564
Kirk Walker Graves <graves.kirk@gmail.com>
2016-02-13 21:34:58 -0600

    added button and functionality for clearing returned search feed

--- app/controllers/SearchCtrl.js
+++ app/controllers/SearchCtrl.js
@@ -1,6 +1,7 @@
 app.controller('SearchCtrl', ['$http', '$stateParams', '$state', function($http, $stateParams, $state) {
 
 	var vm = this;
+	vm.bookTitleList;
 	vm.volumeId = $stateParams.volumeId;
 
 
@@ -14,6 +15,14 @@ app.controller('SearchCtrl', ['$http', '$stateParams', '$state', function($http,
 			console.log('Error: ', Error);
 		});
 
+	};
+
+	vm.clear = function() {
+		vm.titleQuery = "";
+		vm.bookTitleList = {};
+		document.getElementsByName('md-divider') = vm.cleanup;
+		vm.cleanup = null;
+		// Not sure how to get this function to clear the md-divider after the array of returned books is cleared out.
 	};
 
 	vm.selectThisBook = function(book) {
